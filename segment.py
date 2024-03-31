83f57c6a7d8483f4aeb37ffa3ea7af44d8e366e5
Timothy Zhou <z.timothy96@gmail.com>
2024-03-30 20:58:35 -0700

    introduce flexible staff line threshold

--- segment.py
+++ segment.py
@@ -3,6 +3,13 @@ import numpy as np
 import cv2
 import streamlit as st
 
+def plot_histogram(x, thresh):
+    if not st.session_state['debug']:
+        return
+    fig, _ = plt.subplots()
+    plt.hist(x)
+    plt.axvline(x=thresh, color="black", linestyle="--")
+    st.pyplot(fig)
 
 def plot_lines(im, lines, axis=0):
     if not st.session_state['debug']:
@@ -26,14 +33,24 @@ def plot_lines(im, lines, axis=0):
     plt.axis('off')
     st.pyplot(fig)
 
+def find_staff_lines(im_bin):
+    brightness = np.mean(im_bin, axis=1)
+    sorted_brightness = np.sort(brightness)
+    sorted_diff = np.diff(sorted_brightness)
+    i = np.argmax(sorted_diff)
+    thresh = sorted_brightness[i] + sorted_diff[i]/2
+    staff_lines = (brightness < thresh).nonzero()[0]
+    plot_histogram(sorted_brightness, thresh)
+    plot_lines(im_bin, staff_lines, axis=0)
+    return staff_lines
 
-def find_staff_lines(im_bin, window = 50, thresh=0.3):
+def find_staff_borders(im_bin, window = 50):
     H = im_bin.shape[0]
-    staff_lines = (np.mean(im_bin, axis=1) < thresh).nonzero()[0]
-    whiteness = np.mean(im_bin, axis=0)
+    staff_lines = find_staff_lines(im_bin)
+    brightness = np.mean(im_bin, axis=0)
     # get the x coordinate at the start of the staves
     # this can actually be the end of the stave if the first stave is offset...
-    start_x = np.argmin(whiteness) 
+    start_x = np.argmin(brightness)
     plot_lines(im_bin, [start_x], axis=1)
 
     # get the y coordinates at the top and bottom of each system 
@@ -67,7 +84,7 @@ def get_staves(im):
     im_bin = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
     im_bin = cv2.threshold(im_bin*1.0/255, thresh, 1.0, cv2.THRESH_BINARY)[1]
 
-    border_lines = find_staff_lines(im_bin)
+    border_lines = find_staff_borders(im_bin)
     plot_lines(im_bin, border_lines, axis=0)
     seps = find_seps(im_bin, border_lines)
     plot_lines(im_bin, seps, axis=0)
